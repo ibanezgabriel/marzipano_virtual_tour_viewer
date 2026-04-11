db5a30f82321afe2b91384a891e5541d080bf6cf
Xavier Lorenzo <xavierlorenzo@Xaviers-Mac-mini.local>
2026-04-12 01:19:12 +0800

    Make project-viewer responsive + public UX tweaks via MacOS

--- public/js/menu-collapsible.js
+++ public/js/menu-collapsible.js
@@ -1,15 +1,15 @@
 export function initMenuCollapsible() {
   const sidebarBTN = document.getElementById("pano-sidebar-btn");
-  const sidebarIMG = sidebarBTN.querySelector('img');
+  const sidebarIMG = sidebarBTN ? sidebarBTN.querySelector("img") : null;
   const sidebarWrapper = document.getElementById("pano-sidebar-wrapper");
 
   if (sidebarBTN && sidebarWrapper) {
     sidebarBTN.addEventListener("click", () => {
       sidebarWrapper.classList.toggle("collapsed");
       if (sidebarWrapper.classList.contains("collapsed")) {
-        sidebarIMG.src = "../assets/side-bar-show.png";
+        if (sidebarIMG) sidebarIMG.src = "assets/side-bar-show.png";
       } else {
-        sidebarIMG.src = "../assets/side-bar-hide.png";
+        if (sidebarIMG) sidebarIMG.src = "assets/side-bar-hide.png";
       }
     });
   }
